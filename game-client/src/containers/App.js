3f41b200bad4c5b8616d40a70172fbe2d10da972
Harley Bartles <32537371+HarleyBartles@users.noreply.github.com>
2020-02-24 16:39:20 +0000

    folder structure

---
+++ game-client/src/containers/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Layout from '../components/Layout/Layout'
+
+class App extends React.Component() {
+  render(){
+    return (
+      <Layout>
+        <strong>content</strong>
+      </Layout>
+    );
+  }
+  
+}
+
+export default App;
